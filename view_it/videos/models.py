483d92fb2a3074cd5543c11ff39f30d016721aba
Julio Fajardo <jfajardoh5@gmail.com>
2023-05-02 17:10:47 -0400

    Started building Videos model

--- view_it/videos/models.py
+++ view_it/videos/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from view_it.users.models import User
+
+
+class Videos(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_title = models.CharField(max_length=200)
+    video_description = models.TextField()
+    file = models.FileField(upload_to="videos/%Y/%m/%d/%H/")
+    published_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.video_title
