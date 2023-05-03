7e80e5a7f32999ea2827b2233b65365789366e89
Julio Fajardo <jfajardoh5@gmail.com>
2023-05-03 05:39:48 -0400

    Added fontawesome and started establishing the nav bar.

--- view_it/static/js/project.js
+++ view_it/static/js/project.js
@@ -1,3 +1,5 @@
 import '../sass/project.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 
 /* Project specific Javascript goes here. */
