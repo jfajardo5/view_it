0e2eacb13f0a2950801d9106abd01b1d796e7890
Julio Fajardo <jfajardoh5@gmail.com>
2023-05-05 18:54:25 -0400

    Configured for proper path registration

--- view_it/videos/apps.py
+++ view_it/videos/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class VideosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "videos"
+    name = "view_it.videos"
+    verbose_name = _("Videos")
