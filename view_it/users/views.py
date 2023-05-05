f43527307c58c3088dfdf67d94fa3519c171cb9a
Julio Fajardo <jfajardoh5@gmail.com>
2023-05-05 18:43:57 -0400

    Prepared UserDetailView to list users' videos.

--- view_it/users/views.py
+++ view_it/users/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
 
+from view_it.videos.models import Videos
+
 User = get_user_model()
 
 
@@ -12,6 +14,15 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        public_videos = Videos.objects.filter(user=user, status="public")
+        private_videos = Videos.objects.filter(user=user, status="private")
+        context["public_videos"] = public_videos
+        context["private_videos"] = private_videos
+        return context
 
 
 user_detail_view = UserDetailView.as_view()
