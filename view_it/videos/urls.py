73726c00236d2bca1002c5ad72d44613741d0bc0
Julio Fajardo <jfajardoh5@gmail.com>
2023-05-07 09:19:53 -0400

    Created VideoUpdateView

--- view_it/videos/urls.py
+++ view_it/videos/urls.py
@@ -5,6 +5,6 @@ from .views import video_detail_view, video_update_view, video_upload_view
 app_name = "videos"
 urlpatterns = [
     path("upload/", view=video_upload_view, name="upload"),
-    path("~update/", view=video_update_view, name="update"),
+    path("~update/<slug:url_slug>/", view=video_update_view, name="update"),
     path("<slug:url_slug>/", view=video_detail_view, name="detail"),
 ]
--- view_it/videos/views.py
+++ view_it/videos/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DetailView, UpdateView
 
@@ -33,12 +34,18 @@ video_detail_view = VideoDetailView.as_view()
 
 class VideoUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Videos
-    fields = ["video_title", "video_description", "thumbnail", "status"]
+    slug_field = "url_slug"
+    slug_url_kwarg = "url_slug"
+    fields = ["title", "description", "thumbnail", "status"]
+    template_name = "videos/update_video.html"
     success_message = _("Information successfully updated!")
 
     def get_success_url(self):
         assert self.request.user.is_authenticated  # for mypy to know that the user is authenticated
-        return self.request.user.get_absolute_url()
+        return reverse("videos:detail", kwargs={"url_slug": self.object.url_slug})
+
+    def get_object(self):
+        return Videos.objects.filter(url_slug=self.kwargs["url_slug"], user=self.request.user.id).first()
 
 
 video_update_view = VideoUpdateView.as_view()
